1d9d82e3b651db4c48d932fb63ec7f5fa1899296
ishaan2020 <53574413+ishaan2020@users.noreply.github.com>
2021-08-06 10:44:05 -0400

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn education-app.wsgi --log-file -
--- education-app/settings.py
+++ education-app/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-learning-portal-ishaan.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
