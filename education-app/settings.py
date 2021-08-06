144e3255a6ef82b526c5e8df3842e3d46e461dca
ishaan2020 <53574413+ishaan2020@users.noreply.github.com>
2021-08-06 11:09:01 -0400

    first commit

--- education-app/settings.py
+++ education-app/settings.py
@@ -118,6 +118,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
